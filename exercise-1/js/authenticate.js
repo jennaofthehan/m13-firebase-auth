6d92dd8690a7b0a37b826652ba28e2caa42121a5
Jenna Han <jenna3@uw.edu>
2016-10-25 10:13:57 -0700

    class

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -1,15 +1,52 @@
 // JavaScript authentication file
 $(function() {
+	var config = {
+		apiKey: "AIzaSyDpsLDR3ClbFD076UGEsWdZC6C_KA-8DY4",
+		authDomain: "m13-ex1-ce6a7.firebaseapp.com",
+		databaseURL: "https://m13-ex1-ce6a7.firebaseio.com",
+		storageBucket: "m13-ex1-ce6a7.appspot.com",
+		messagingSenderId: "155683451358"
+	};
 
-    // Initialize Firebase
+    firebase.initializeApp(config);
+
+    var myRef = firebase.database().ref('ref-id');
+
+    $("form").on("submit", function(event) {
+    	event.preventDefault();
+    	if(this.id == "sign-up"){
+    		signUp();
+    	}
+    });
+
+    $("#sign-in").on("click", function(event) {
+    	event.preventDefault();
+    	window.location = "sign-in.html";
+    })
+
+
 
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
         // Get email, password, and display name
 
-
         // Create user, then set the user's display name
+        var email = $("#email").val();
+        var pass = $("#password").val();
+        var dispName = $("#display").val();
 
+        alert(email + " " + pass + " " + dispName);
+
+		// var auth = firebase.auth();
+		firebase.auth().createUserWithEmailAndPassword(email, pass).then(function(user){
+			alert("createUserWithEmailAndPassword");
+		    user.updateProfile({
+		        displayName: dispName
+		    }).then(function() {
+		    	window.location = "index.html";
+		    })
+		    alert("inside create user");
+		})
                 // Set display name
 
     };
@@ -17,6 +54,7 @@ $(function() {
     // SignIn: Function to authenticate on Firebase, then redirect to index.html
     var signIn = function() {
         // Get email and password
+
 
 
         // Authenticate using email and password, then redirect
